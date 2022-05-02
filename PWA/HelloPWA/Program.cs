a579b9b94c899842169a583324abf6aa1f888ab0
Ali Reza Farahnak @ Microsoft <52029052+alfarahn@users.noreply.github.com>
2022-05-02 17:10:31 +0000

    m

--- PWA/HelloPWA/Program.cs
+++ PWA/HelloPWA/Program.cs
@@ -1,11 +1,15 @@
+using Azure.Messaging.ServiceBus;
 using HelloPWA;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+string connectionString = "connectionString";
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(new ServiceBusClient(connectionString));
 
 await builder.Build().RunAsync();
