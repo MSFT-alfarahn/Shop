997eea8536ee37c45b672fc93919117229fe13a5
Ali Reza Farahnak @ Microsoft <52029052+alfarahn@users.noreply.github.com>
2022-05-02 13:38:21 +0000

    Initial Application

---
+++ Shop/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Shop;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Shop/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Shop;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
