c6e78516b44dae874658d9666df18f6b19f9ce7a
Ali Reza Farahnak @ Microsoft <52029052+alfarahn@users.noreply.github.com>
2022-05-04 09:33:41 +0000

    m

---
+++ Shop/Model/Fruits/Template/!FruitTemplateSelector.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Shop.Model.Fruits.Template;
+
+public class TechItemTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate DefaultTemplate { get; set; }
+    public DataTemplate GuavaTemplate { get; set; }
+    public DataTemplate WatermelonTemplate { get; set; }
+    public DataTemplate BananasTemplate { get; set; }
+
+    protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+    {
+        if (item.GetType() == typeof(Guava))
+            return GuavaTemplate;
+        else if (item.GetType() == typeof(WatermelomTemplate))
+             return WatermelonTemplate;
+        else if (item.GetType() == typeof(BananasTemplate))
+            return BananasTemplate;
+        else
+            return BananasTemplate;
+    }
+}
