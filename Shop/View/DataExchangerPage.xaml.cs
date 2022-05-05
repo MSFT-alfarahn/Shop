34d39f04a3b9fd14db3ea5bfae7df25140e3d768
Ali Reza Farahnak @ Microsoft <52029052+alfarahn@users.noreply.github.com>
2022-05-05 14:57:00 +0000

    implemented validation example

--- Shop/View/DataExchangerPage.xaml.cs
+++ Shop/View/DataExchangerPage.xaml.cs
@@ -1,3 +1,5 @@
+using CommunityToolkit.Maui.Behaviors;
+
 namespace Shop;
 
 public partial class DataExchangerPage : ContentPage
@@ -6,5 +8,42 @@ public partial class DataExchangerPage : ContentPage
 	{
 		InitializeComponent();
 		BindingContext = vm;
+
+		AddEntryValidation();
 	}
+
+    private void AddEntryValidation()
+    {
+        var entry = new Entry
+        {
+            Keyboard = Keyboard.Numeric
+        };
+
+        var validStyle = new Style(typeof(Entry));
+        validStyle.Setters.Add(new Setter
+        {
+            Property = Entry.TextColorProperty,
+            Value = Colors.Green
+        });
+
+        var invalidStyle = new Style(typeof(Entry));
+        invalidStyle.Setters.Add(new Setter
+        {
+            Property = Entry.TextColorProperty,
+            Value = Colors.Red
+        });
+
+        var numericValidationBehavior = new NumericValidationBehavior
+        {
+            InvalidStyle = invalidStyle,
+            ValidStyle = validStyle,
+            Flags = ValidationFlags.ValidateOnValueChanged,
+            MinimumValue = 1.0,
+            MaximumValue = 100.0,
+            MaximumDecimalPlaces = 2
+        };
+
+        entry.Behaviors.Add(numericValidationBehavior);
+      //  stack.Children.Add(entry);
+    }
 }
