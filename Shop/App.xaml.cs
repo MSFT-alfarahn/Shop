9055a09de56f992257bddd7b925a79567c7496e8
Ali Reza Farahnak @ Microsoft <52029052+alfarahn@users.noreply.github.com>
2022-05-05 13:42:00 +0000

    Idea on how to do offline sync

--- Shop/App.xaml.cs
+++ Shop/App.xaml.cs
@@ -8,4 +8,24 @@ public partial class App : Application
 
 		MainPage = new MyTabs(); //The tabs are now based on Shell and all the benefits!
 	}
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+
+        // here you look up local db and sync with online db
+        
+        
+        // Hook up to an event whenever there is connectivity then you can resume sync
+        // You could use Essentials for this:
+        //    // Register for connectivity changes, be sure to unsubscribe when finished
+        //Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+    }
+
+    protected override void OnSleep()
+    {
+        base.OnSleep();
+
+        // here you will pause the offline sync 
+    }
 }
